dda701c4e3b6d45adcc70b3342d79a0bdb2d5829
Fabian <draizfabian@gmail.com>
2021-03-16 15:27:56 +0200

    working app to make changes on

---
+++ main/scraper/scraper/items.py
@@ -0,0 +1,11 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+from scrapy_djangoitem import DjangoItem
+from properties.models import Property
+
+
+class ScraperItem(DjangoItem):
+    django_model = Property
