b0706125f05b48c3854f328930fe5bb1b4d91c11
Fabian <draizfabian@gmail.com>
2021-03-17 02:19:24 +0200

    working bbc crawler

--- main/scraper/scraper/spiders/properties_spider.py
+++ main/scraper/scraper/spiders/properties_spider.py
@@ -3,11 +3,21 @@ from scrapy.spiders import Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.loader import ItemLoader
 from scrapy.loader.processors import TakeFirst
-
 from scraper.scraper.items import ScraperItem
 
+from scrapy.spiders import SitemapSpider
+from django.db import models
 
-class PropertiesSpider(CrawlSpider):
+class PropertiesSpider(SitemapSpider):
+    name = 'crawlbbc'
+    allowed_domains=['bbc.com']
+    sitemap_urls = ['https://www.bbc.com/sitemaps/https-index-com-news.xml']
+#    custom_settings = { 'CLOSESPIDER_PAGECOUNT': 10,'ROBOTSTXT_OBEY' : True, }
+    def parse(self, response):
+        #image=response.xpath("//span[@class='hero-image']/picture/img").xpath('@src').extract_first()
+        yield ScraperItem( name= response.xpath("//title/text()").get(), url= response.url,body=response.xpath('//p/text()').getall())
+"""
+
     name = "properties"
     allowed_domains = ["realestatedatabase.net"]
     start_urls = [
@@ -65,4 +75,4 @@ class PropertiesSpider(CrawlSpider):
             "agent_company", "span#ContentPlaceHolder1_FormView1_CompanyLabel::text"
         )
 
-        yield property_loader.load_item()
+        yield property_loader.load_item()"""
