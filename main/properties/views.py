afd54f34897d1a068b7dfc940f401d51095183f2
Fabian <draizfabian@gmail.com>
2021-03-19 23:22:01 +0200

    showing Articles out of database

--- main/properties/views.py
+++ main/properties/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from scraper.management.commands import crawl
+from properties.models import Property
 
-# Create your views here.
+
+def news_list(request):
+    print("news_list called")
+    headlines=Property.objects.all()[:10]
+    context={
+    'object_list':headlines
+    }
+    return render(request,"news.html",context)
+
+def scrape(request):
+    pass
+def train_lda(request):
+    
