ebd78f81dcc4bdf42647e5b38f4ff50f940af254
Fabian <draizfabian@gmail.com>
2021-04-02 00:10:53 +0300

    jensen shannon calculation

--- main/properties/preprocess.py
+++ main/properties/preprocess.py
@@ -6,9 +6,35 @@ from gensim.utils import simple_preprocess
 import gensim.corpora as corpora
 import pickle
 from gensim import models, similarities
-
+import numpy as np
+import scipy
 stop_words = stopwords.words('english')
 nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+def jensen_shannon_distance(p, q):
+    """
+    method to compute the Jenson-Shannon Distance
+    between two probability distributions
+    """
+    #print('REACHED')
+    # convert the vectors into numpy arrays in case that they aren't
+    p = np.array(p)
+    q = np.array(q)
+    if len(p)>len(q):
+        dif=len(p)-len(q)
+        arr=np.zeros(dif)
+        q=np.append(q,arr)
+    elif len(p)<len(q):
+        dif=len(q)-len(p)
+        arr=np.zeros(dif)
+        p=np.append(p,arr)
+    # calculate m
+    m = (p + q) / 2
+    #print('REACHED2')
+    # compute Jensen Shannon Divergence
+    divergence = (scipy.stats.entropy(p, m) + scipy.stats.entropy(q, m)) / 2
+    # compute the Jensen Shannon Distance
+    distance = np.sqrt(divergence)
+    return distance
 def sent_to_words(sentences):
     for sentence in sentences:
         yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))
@@ -18,7 +44,7 @@ class clean():
         data = [re.sub('\S*@\S*\s?', '', sent) for sent in data]
         data = [re.sub('\s+', ' ', sent) for sent in data]
         data = [re.sub("\'", "", sent) for sent in data]
-        print('DATA AFTER FIRST PART  ',data)
+        #print('DATA AFTER FIRST PART  ',data)
         data_words = list(sent_to_words(data))
         def remove_stopwords(texts):
             return [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
@@ -33,15 +59,30 @@ class clean():
            'NOUN', 'ADJ', 'VERB', 'ADV'
         ])
         print('data_lemmatized',data_lemmatized)
-        dict = corpora.Dictionary.load('properties/lda/lda_1835/lda_10_1835/modelall.id2word')
+        dict = corpora.Dictionary.load('properties/lda/lda2450_30/model30.id2word')
         converted = [dict.doc2bow(text) for text in data_lemmatized]
-        print('data ready ',converted)
+        #print('data ready ',converted)
         return converted
 
     def get_similarity(lda, query_vector):
         corpus=0
-        with open('properties/lda/lda_1835/lda_10_1835/corpusall', 'rb') as f:
+        with open('properties/lda/lda2450_30/corpus30', 'rb') as f:
             corpus = pickle.load(f)
         index = similarities.MatrixSimilarity(lda[corpus])
         sims = index[query_vector]
         return sims
+
+    def get_jensen_shannon(lda,vector):
+        corpus=0
+        with open('properties/lda/lda2450_30/corpus30', 'rb') as f:
+            corpus = pickle.load(f)
+        vector=[item[1] for item in vector]
+        print("VECTOR AFTER PROCESS")
+        print(vector)
+        vectors=lda[corpus]
+        dists=[]
+        for i in range(len(corpus)):
+            next=[item[1] for item in vectors[i]]
+            dists.append(jensen_shannon_distance(vector,next))
+        #print(dists)
+        return dists
