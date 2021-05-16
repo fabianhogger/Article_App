76b71361168e7fe3c4568aefb8ef64783d535593
Fabian <draizfabian@gmail.com>
2021-05-16 15:49:18 +0300

    changes to include users

--- main/main/settings.py
+++ main/main/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'scraper',
     'properties',
     'phone_field',
+    'users',
 
 ]
 
@@ -94,18 +95,18 @@ MEDIA_URL= "/media/"
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
+    #{
+    #    'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    #},
+    #{
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
+    #},
+    #{
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
+    #},
+    #{
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    #},
 ]
 
 
