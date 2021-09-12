89f55ced0c65aad60d3ab5244cb4ef3d3f24cb1f
Fabian <draizfabian@gmail.com>
2021-09-12 18:00:51 +0300

    all user staff done

--- main/users/urls.py
+++ main/users/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url,include
-from users.views import dashboard
-
+from users.views import dashboard, register
+from . import views
+from properties import views as viewsp
 urlpatterns = [
     url(r"accounts/", include("django.contrib.auth.urls")),
-    url(r"^dashboard/", dashboard, name="dashboard"),
+    url("dashboard/", viewsp.news_list, name="dashboard"),
+    url(r"register/", views.register, name="register"),
 ]
